6344feec6dbb41f4013f847fa0c47de6e9bf8a57
Swap-20 <155949552+Swap-20@users.noreply.github.com>
2025-04-03 20:37:20 +0530

    Fixed Bugs and Added new function

--- index.cpp
+++ index.cpp
@@ -18,15 +18,15 @@ class Account{
     Account() : AccountNo(""),Password(""),Balance(0){}
 
     //value intialization
-    setAccountNo(string id){
+    void setAccountNo(string id){
         AccountNo = id;
     }
     //value intialization
-    setPassword(string pw){
+    void setPassword(string pw){
         Password = pw;
     }
     //value intialization
-    setBalance(int balance){
+    void setBalance(int balance){
         Balance = balance;
     }
 
@@ -42,7 +42,7 @@ class Account{
 
 };
 
-void openAccount(Account user){
+void openAccount(Account &user){
     system("cls");
     string id, pw;
     cout<<"\tEnter Account No : ";
@@ -55,16 +55,125 @@ void openAccount(Account user){
 
     user.setBalance(0);
 
-    ofstream outfile("D:/Account.txt", ios::app);
+    // ofstream to write data in file
+    ofstream outfile("Account.txt", ios::app);
     if(!outfile){
         cout<<"\tError : File Can't Open!"<<endl;
     }else{
-        outfile<<"\t"<<user.getAccountNo()<<" : "<<user.getPassword()<<" : "<<user.getBalance()<<endl;
+        outfile<<"\t"<<user.getAccountNo()<<" : "<<user.getPassword()<<" : "<<user.getBalance()<<endl<<endl;
         cout<<"\tAccount Created Successfully!"<<endl;
     }
     outfile.close();
 
     Sleep(5000);
+}
+
+void addCash(Account &user){
+    system("cls");
+    string id;
+    cout<<"\tEnter Account No : ";
+    cin>>id;
+
+    // ifstream to read the data
+    ifstream infile("Account.txt");
+    ofstream outfile("Account Temp.txt");
+
+    if(!infile || !outfile){
+        cout<<"\tError : File Can't Open"<<endl;
+    }
+
+    string line;
+    bool found = false;
+
+    while(getline(infile, line)){
+        stringstream ss;
+        ss<<line;
+        string userID, userPW;
+        int userBalance;
+        char delimiter;
+
+        ss>>userID>>delimiter>>userPW>>delimiter>>userBalance;
+        if(id == userID){
+            found = true;
+            int cash;
+            cout<<"\tEnter Cash : ";
+            cin>>cash;
+
+            int newBalance = userBalance + cash;
+            userBalance = newBalance;
+            outfile<<"\t"<<userID<<" : "<<userPW<<" : "<<userBalance<<endl;
+            cout<<"\tNew Balance : "<<userBalance<<endl;
+        }
+        else{
+            outfile<<line<<endl;
+        }
+    }
+    if(!found){
+        cout<<"\tEnter valid Account no!"<<endl;
+    }
+    outfile.close();
+    infile.close();
+    remove("Account.txt");
+    rename("Account Temp.txt","Account.txt");
+    Sleep(5000);
+}
+
+void withdraw(){
+    string id,pw;
+    cout<<"\tEnter Account No : ";
+    cin>>id;
+
+    cout<<"\tEnter Password : ";
+    cin>>pw;
+
+    ifstream infile("Account.txt");
+    ofstream outfile("Account Temp.txt");
+    if(!infile || !outfile){
+        cout<<"\tError : File Can't Open!"<<endl;
+    }
+
+    string line;
+    bool found = false;
+    while(getline(infile,line)){
+        stringstream ss;
+        ss<<line;
+        string userID, userPW;
+        int userBalance;
+        char delimiter;
+
+        ss>>userID>>delimiter>>userPW>>delimiter>>userBalance;
+
+        if(id==userID && pw==userPW){
+            found = true;
+            int cash;
+            cout<<"\tEnter Cash : ";
+            cin>>cash;
+            if(cash <= userBalance){
+                int newBalance = userBalance - cash;
+                userBalance = newBalance;
+                outfile<<"\t"<<userID<<" : "<<userPW<<" : "<<userBalance<<endl;
+                cout<<"\tTransaction was Successful!"<<endl;
+                cout<<"\tRemaining Balance : "<<userBalance<<endl;
+            }
+            else{
+                cout<<"\tLow Balance!"<<endl;
+                outfile<<"\t"<<userID<<" : "<<userPW<<" : "<<userBalance<<endl;
+            }
+        }
+        else{
+            outfile<<line<<endl;
+        }
+    }
+    if(!found){
+        cout<<"\tInvalid Accound or Password!"<<endl;
+    }
+    outfile.close();
+    infile.close();
+    remove("Account.txt");
+    rename("Account Temp.txt","Account.txt");
+    Sleep(5000);
+
+
 }
 int main(){
     Account user;
@@ -82,7 +191,7 @@ int main(){
         cout<<"\t2.Add Cash."<<endl;
         cout<<"\t3.Withdraw Cash."<<endl;
         cout<<"\t4.Exit."<<endl;
-        cout<<"\tEnter Your Choice : "<<endl;
+        cout<<"\tEnter Your Choice : ";
 
         cin>>val;
         // cin : scan or get the user input
@@ -90,6 +199,19 @@ int main(){
         if(val==1){
             openAccount(user);
         }
+        else if(val==2){
+            addCash(user);
+        }
+        else if(val==3){
+            withdraw();
+        }
+        else if(val==4){
+            system("cls");
+            exit = true;
+            cout<<"\tGood Luck!"<<endl;
+            Sleep(2000);
+        }
         Sleep(3000);
     }
 }
+
